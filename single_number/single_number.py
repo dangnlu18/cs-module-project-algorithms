1dbf9cfda706d562116269f94ba24f3541122809
dang lu <danglu@dangs-MBP.lan>
2020-07-14 19:45:41 -0700

    single number complete

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,9 +4,19 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    #create char map to count number of times char appears in array
+    char_map = {}
+    for val in arr:
+        if val in char_map:
+            char_map[val] +=1
+        else:
+            char_map[val] =1
+    
+    for key in char_map:
+        if char_map[key] ==1:
+            return key
 
-    pass
-
+single_number([1,2,3,4,4,3,2])
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
